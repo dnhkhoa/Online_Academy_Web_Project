b897675631e5fc86261b74488cfdff4639d99c5f
KhanzzOnlySp <khanganduc@gmail.com>
2025-10-03 15:12:26 +0700

    feat: add signin, signup, homepage

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import hsb_sections from "express-handlebars-sections";
 
 const __dirname = import.meta.dirname;
 const app = express();
+
+app.engine(
+  "handlebars",
+  engine({
+    helpers: {
+      section: hsb_sections(),
+      format_number(value) {
+        return new Intl.NumberFormat("en-US").format(value);
+      },
+    },
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static("static"));
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.render("home");
 });
+
+import accountRouter from "./routes/account.route.js";
+app.use("/account", accountRouter);
 
 app.listen(3000, function () {
   console.log("Server is running on http://localhost:3000");
